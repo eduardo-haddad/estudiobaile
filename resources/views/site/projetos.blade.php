494a650d027b8f5c2df6ae3f9cf5ff1714eab503
zerinoid <leo.zerino@gmail.com>
2019-07-23 04:17:08 -0300

    pagina projetos completa

--- resources/views/site/projetos.blade.php
+++ resources/views/site/projetos.blade.php
@@ -1,165 +1,174 @@
 @extends('site.layouts.base')
 @section('conteudo')
 
-<div class="row justify-content-center" id="projetos">
-  <div class="col-md-8">
-    <div class="row no-gutters">
-      <div class="col-md-12" id="legendas">
-        <div class="leg-item">
-          <img src="{{ asset('/images/passado.png') }}" alt="">
-          <p>
-            passado
-          </p>
-        </div>
-        <div class="leg-item">
-          <img src="{{ asset('/images/presente.png') }}" alt="">
+
+  <div class="row justify-content-center projetos">
+    <div class="col-md-8" id="legendas">
+      <div class="leg-item">
+        <img src="{{ asset('/images/passado.png') }}" alt="">
+        <p>
+          passado
+        </p>
+      </div>
+      <div class="leg-item">
+        <img src="{{ asset('/images/presente.png') }}" alt="">
+        <p>
           presente
-        </div>
-        <div class="leg-item">
-          <img src="{{ asset('/images/futuro.png') }}" alt="">
-          futuro
-        </div>
-        <div class="leg-item cap-c">
-          <img src="{{ asset('/images/externo.png') }}" alt="">
-          externos
-        </div>
+        </p>
+      </div>
+      <div class="leg-item">
+        <img src="{{ asset('/images/futuro.png') }}" alt="">
+        <p>
+          futuros
+        </p>
+      </div>
+      <div class="leg-item cap-c">
+        <img src="{{ asset('/images/externo.png') }}" alt="">
+        <p>
+          projetos tipo c
+        </p>
+      </div>
+      <hr>
+    </div>
+  </div>
+  <div class="row justify-content-center projetos">
+    <div class="col-md-2">
+      <div class="thu-projeto futuro">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
+      </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag + tag</p>
       </div>
     </div>
-    <div class="row no-gutters" id="proj-gal">
-      <div class="col-md-3">
-        <div class="thu-projeto futuro">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag + tag</p>
-        </div>
+    <div class="col-md-2">
+      <div class="thu-projeto presente">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto presente">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption cap-c">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag</p>
-        </div>
+      <div class="caption cap-c">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag</p>
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto passado">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag + tag + tag</p>
-        </div>
+    </div>
+    <div class="col-md-2">
+      <div class="thu-projeto passado">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto passado">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag + tag + tag</p>
-        </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag + tag + tag</p>
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto presente">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag</p>
-        </div>
+    </div>
+    <div class="col-md-2">
+      <div class="thu-projeto passado">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto presente">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag</p>
-        </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag + tag + tag</p>
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto futuro">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag + tag</p>
-        </div>
+    </div>
+    <div class="w-100"></div>
+    <div class="col-md-2">
+      <div class="thu-projeto presente">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto passado">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag + tag + tag</p>
-        </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag</p>
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto passado">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag + tag + tag</p>
-        </div>
+    </div>
+    <div class="col-md-2">
+      <div class="thu-projeto presente">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto passado">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag + tag + tag</p>
-        </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag</p>
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto passado">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag + tag + tag</p>
-        </div>
+    </div>
+    <div class="col-md-2">
+      <div class="thu-projeto futuro">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
       </div>
-      <div class="col-md-3">
-        <div class="thu-projeto futuro">
-          <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
-        </div>
-        <div class="caption">
-          <p class="bold">
-            Nome do Projeto
-          </p>
-          <p>tag</p>
-        </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag + tag</p>
+      </div>
+    </div>
+    <div class="col-md-2">
+      <div class="thu-projeto passado">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
+      </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag + tag + tag</p>
+      </div>
+    </div>
+    <div class="w-100"></div>
+    <div class="col-md-2">
+      <div class="thu-projeto passado">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
+      </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag + tag + tag</p>
+      </div>
+    </div>
+    <div class="col-md-2">
+      <div class="thu-projeto passado">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
+      </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag + tag + tag</p>
+      </div>
+    </div>
+    <div class="col-md-2">
+      <div class="thu-projeto passado">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
+      </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag + tag + tag</p>
+      </div>
+    </div>
+    <div class="col-md-2">
+      <div class="thu-projeto futuro">
+        <img src="{{ asset('images/galeria1/MissBaker.png') }}" alt="">
+      </div>
+      <div class="caption">
+        <p class="bold">
+          Nome do Projeto
+        </p>
+        <p>tag</p>
       </div>
     </div>
   </div>
-</div>
+  {{-- <div class="col-md-8">
+  </div> --}}
+
 
 @stop
