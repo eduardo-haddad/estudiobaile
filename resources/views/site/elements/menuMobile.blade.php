f602dc11431a797694d0015d84fb79359c8ad1eb
zerinoid <leo.zerino@gmail.com>
2019-08-13 19:12:08 -0300

    arquivos mobile não inclusos em commits anteriores

---
+++ resources/views/site/elements/menuMobile.blade.php
@@ -0,0 +1,32 @@
+<div id="menuMobile" class="container-fluid d-none">
+  <div class="row justify-content-center">
+    <div class="col-6">
+      <p><a href="#">PT</a> / <a href="#">EN</a></p>
+    </div>
+    <div class="col-6 redesMobile">
+      <div class="tooltipster icon loc" data-tooltip-content="#tooltip_content"></div>
+      <div class="icon">
+        <a href="https://www.facebook.com/baile.estudio/" target="_blank">
+          <img src="/images/mobile/fb.png" alt="">
+        </a>
+      </div>
+      <div class="icon">
+        <a href="https://www.instagram.com/estudio_baile/" target="_blank">
+          <img src="/images/mobile/insta.png" alt="">
+        </a>
+      </div>
+      <div class="icon toggleMenu">
+        <img src="/images/mobile/fechar.png" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col naveMobile"><a href="{{ url ('/site/sobre') }}" class="toggleMenu">sobre</a></div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col naveMobile"><a href="{{ url ('/site/projetos') }}" class="toggleMenu">projetos</a></div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col naveMobile"><a href="{{ url ('/site/agenda') }}" class="toggleMenu">agenda</a></div>
+  </div>
+</div>
