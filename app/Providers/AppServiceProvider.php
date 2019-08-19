f5aae5d1318ed1cb1c47df4186fab944c422055d
zerinoid <leo.zerino@gmail.com>
2019-08-19 20:36:55 -0300

    teste de alias para component

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Blade;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::component('site.components.notas', 'notas');
     }
 
     /**
