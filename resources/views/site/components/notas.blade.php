8eea4c6b0c3d468ab6fe07a5dcc62c676fbccce1
zerinoid <leo.zerino@gmail.com>
2019-08-19 20:35:19 -0300

    carousel projetos mobile;  incremento na modularização

--- resources/views/site/components/notas.blade.php
+++ resources/views/site/components/notas.blade.php
@@ -1,14 +1,16 @@
-<div class="notas">
-  <p class="bold">
-    1
-  </p>
-  <p>
-    fulano de tal
-  </p>
-  <p>
-    / 
-  </p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, necessitatibus.</p>
-  <p>Debitis itaque sed odio error eos, adipisci temporibus nobis ipsum!</p>
-  <p>Laborum debitis recusandae non fugit mollitia. Error qui quo accusamus.</p>
+<div class="col-md-3 col-fix-3 d-none d-md-block">
+  <div class="notas">
+    <p class="bold">
+      1
+    </p>
+    <p>
+      fulano de tal
+    </p>
+    <p>
+      / 
+    </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, necessitatibus.</p>
+    <p>Debitis itaque sed odio error eos, adipisci temporibus nobis ipsum!</p>
+    <p>Laborum debitis recusandae non fugit mollitia. Error qui quo accusamus.</p>
+  </div>
 </div>
