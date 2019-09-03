a2a73d41183bb420094b4886625df9ee817b471b
zerinoid <leo.zerino@gmail.com>
2019-09-03 00:31:05 -0300

    correção no component 'galeria'

--- resources/views/site/components/galeria.blade.php
+++ resources/views/site/components/galeria.blade.php
@@ -7,31 +7,21 @@
         @foreach ($ibagens as $i => $imagem)
           
           @php
-            //orientação de acordo com a altura e a largura da imagem
 
-            // list($width, $height) = getimagesize("$_SERVER[DOCUMENT_ROOT]$imagem");
+            //0 ou 1, randômico
+            if($i % 2 == 0){
+              $rand = mt_rand(0, 1);  
+            } 
             
-            // if ($width > $height) {
-            //   $orientacao = "landscape";
-            // } else {
-            //   $orientacao = "portrait";
-            // }
+            $orientacao = ["portrait", "landscape"];
+            $classe = $orientacao[$rand];
 
-            // TODO orientação randomica (2 thumbs por iteração)
-            
-            $randOrient = rand(0, 1);
-
-            // if ($i % 2 != 0) {
-            $orientacao = ($randOrient == 0 ? "portrait" : "landscape");
-            // } else {
-            $orientacao2 = ($randOrient == 1 ? "portrait" : "landscape");
-            // }
             $legenda = "Legenda";
             $credito = "Crédito";
-            
+
           @endphp
             
-          <div class="thumb {{ $orientacao }}">
+          <div class="thumb {{ $classe }}">
             <div class="hoverThumb transparente">
               <div class="captionGaleria">
                 <p>{{ $legenda }}</p>
@@ -43,49 +33,14 @@
             </a>
           </div>
 
-          <div class="thumb {{ $orientacao2 }}">
-            <div class="hoverThumb transparente">
-              <div class="captionGaleria">
-                <p>{{ $legenda }}</p>
-                <p>{{ $credito }}</p>
-              </div>
-            </div>
-            <a href="{{ $imagem }} " class="swipebox" title="caption">
-              <img src="{{ $imagem }}" alt="image" class="imgProj">
-            </a>
-          </div>
+          @php
+
+            $rand = !$rand;
+
+          @endphp
             
         @endforeach
-        {{-- <div class="thumb landscape">
-          <a href="{{ asset('/images/galeria1/RM2.png') }}" class="swipebox" title="caption">
-            <img src="{{ asset('/images/galeria1/RM2.png') }}" alt="image" class="imgProj">
-          </a>
-        </div>
-        <div class="thumb portrait">
-          <a href="{{ asset('/images/galeria1/RM1.png') }}" class="swipebox" title="caption">
-            <img src={{ asset('/images/galeria1/RM1.png') }} class="ble">
-          </a>
-        </div>
-        <div class="thumb portrait">
-          <a href="{{ asset('/images/galeria1/RM4.jpg') }}" class="swipebox" title="caption">
-            <img src={{ asset('/images/galeria1/RM4.jpg') }}>
-          </a>
-        </div>
-        <div class="thumb landscape">
-          <a href="{{ asset('/images/galeria1/RM5.jpg') }}" class="swipebox" title="caption">
-            <img src={{ asset('/images/galeria1/RM5.jpg') }}>
-          </a>
-        </div>
-        <div class="thumb portrait">
-          <a href="{{ asset('/images/galeria1/RM2.png') }}" class="swipebox" title="caption">
-            <img src={{ asset('/images/galeria1/RM2.png') }}>
-          </a>
-        </div>
-        <div class="thumb landscape">
-          <a href="{{ asset('/images/galeria1/RM1.png') }}" class="swipebox" title="caption">
-            <img src={{ asset('/images/galeria1/RM1.png') }}>
-          </a>
-        </div> --}}
+
       </div>
     </div>
   </div>
