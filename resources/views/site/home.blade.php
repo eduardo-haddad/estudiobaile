93e90dccaa2d00479892d896cd5e91cfafe75920
zerinoid <leo.zerino@gmail.com>
2019-09-06 01:01:27 -0300

    scrollbar nas listas da home

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -20,15 +20,6 @@
           </div>
         @endforeach
       </div>
-
-      {{-- <a class="carousel-control-prev" href="#carouselHome" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselHome" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a> --}}
     </div>
 
   </div>
@@ -51,6 +42,11 @@
         <ul class="lista" id="lista1">
           <li><a>assunto</a></li>
           <li><a>lugar</a></li>
+          <li><a>parceria</a></li>
+          <li><a>parceria</a></li>
+          <li><a>parceria</a></li>
+          <li><a>parceria</a></li>
+          <li><a>parceria</a></li>
           <li><a>parceria</a></li>
           <li><a>pessoa</a></li>
         </ul>
